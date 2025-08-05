7b9bca3620fb9cb22dd24142152e3d7b694ff27e
Ajay Prawin <ajayprawinsk@gmail.com>
2025-08-05 09:34:51 +0530

    Add files via upload

---
+++ team_avg_strike_rate.cs
@@ -0,0 +1,9 @@
+team_id,team_name,avg_strike_rate
+101,India,87.88
+102,"South Africa",80.07
+103,"New Zealand",91.33
+104,Australia,100.96
+105,Afghanistan,75.64
+106,England,87.82
+107,Bangladesh,50.74
+108,Pakistan,77.30
